27d5c5cd4e9d270d37466e9124d7acbb45641e15
No767 <73260931+No767@users.noreply.github.com>
2022-06-29 23:49:05 -0700

    add help cmd

--- Bot/Cogs/help.py
+++ Bot/Cogs/help.py
@@ -1,5 +1,6 @@
-from discord.commands import slash_command
-from discord.ext import commands
+import discord
+from discord.commands import Option, slash_command
+from discord.ext import commands, pages
 
 
 class MainHelpV1(commands.Cog):
@@ -11,21 +12,56 @@ class MainHelpV1(commands.Cog):
         description="Provides the help page for Miku",
         guild_ids=[978546162745348116],
     )
-    async def mikuHelp(self, ctx):
-        for items in self.bot.walk_application_commands():
-            print(items.name)
-            print(items.description)
-            # print(items.module)
-
-            # print(dir(items))
-        # cogs = self.bot.get_cog("MainHelpV1")
-        # commands = cogs.get_commands()
-        # print(commands)
-        # for cog, commands in mapping.items():
-        #     cogs = self.bot.get_cog("MainHelpV1")
-        #     commands = cog.get_commands()
-        #     print([c.name for c in commands])
-        await ctx.respond("testing")
+    async def mikuHelp(
+        self,
+        ctx,
+        *,
+        command: Option(
+            str,
+            "The command to search. This does not include subcommands.",
+            required=False,
+        ),
+    ):
+        if command is None:
+            mainPages = pages.Paginator(
+                pages=[
+                    discord.Embed(
+                        title=items.name, description=items.description
+                    ).add_field(
+                        name="Parent Name",
+                        value=f"[{items.full_parent_name}]",
+                        inline=True,
+                    )
+                    for items in self.bot.walk_application_commands()
+                ]
+            )
+            await mainPages.respond(ctx.interaction, ephemeral=False)
+        else:
+            commandItem = self.bot.get_application_command(command)
+            if commandItem is None:
+                await ctx.respond(
+                    embed=discord.Embed(
+                        description="Sorry, but that command was not found. Please try again"
+                    )
+                )
+            else:
+                embed = discord.Embed()
+                if len(commandItem.options) == 0:
+                    commandItemOptions = None
+                else:
+                    commandItemOptionsMain = commandItem.options
+                    for items in commandItemOptionsMain:
+                        commandItemOptions = items.name
+                embed.title = commandItem.qualified_name
+                embed.description = commandItem.description
+                embed.add_field(name="Options", value=commandItemOptions, inline=True)
+                embed.add_field(
+                    name="Parent Command",
+                    value=f"[{commandItem.full_parent_name}]",
+                    inline=True,
+                )
+                embed.add_field(name="Type", value=commandItem.type, inline=True)
+                await ctx.respond(embed=embed)
 
 
 def setup(bot):
